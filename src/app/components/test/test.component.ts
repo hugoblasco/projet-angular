df7491f4f9bca5a2df05ff15ba16191f73781763
JeffRobbe <jfrobbe96@gmail.com>
2019-09-17 13:44:39 +0200

    bootstrap added

---
+++ src/app/components/test/test.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { MovieApiService } from 'src/app/services/movie-api.service';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.scss']
+})
+export class TestComponent implements OnInit {
+  trending = [];
+
+  constructor(
+    private movieApiService: MovieApiService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  getTrending() {
+    this.movieApiService.getTrending()
+    .subscribe((res: any) => {
+      console.log(res);
+      this.trending = res.results;
+    }, err => {
+      console.log(err);
+    });
+  }
+
+}
---
+++ src/app/services/movie-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+const apiKey = '6ad0c1332948999e8a178c3fd93d2567';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieApiService {
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  getTrending(): Observable<any> {
+    return this.httpClient.get('https://api.themoviedb.org/3/trending/all/day?api_key=' + apiKey);
+  }
+}
